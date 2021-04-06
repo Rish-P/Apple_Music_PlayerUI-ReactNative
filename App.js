999e7c7e5c250ceb4ae8dc48052922548a910c21
Rish-P <ar.pee@aol.com>
2021-04-06 16:33:38 +0530

    few files changed

--- App.js
+++ App.js
@@ -1,223 +1,163 @@
-import React, { Component, useRef, useState } from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet, Dimensions } from 'react-native'
-import { TextInput, TapGestureHandler, State } from 'react-native-gesture-handler';
-import Animated, { useCode, cond, set, eq, Easing, interpolate, SpringUtils } from 'react-native-reanimated'
-import { timing, useTimingTransition, withTimingTransition, onGestureEvent, spring, withSpringTransition } from 'react-native-redash/lib/module/v1'
-import { Ionicons } from '@expo/vector-icons'
+import React, { useState, useRef } from 'react';
+import {
+  View, Text, StyleSheet, Animated, TouchableOpacity,
+  Dimensions,
+  Image,
+  PanResponder,
+} from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import Slider from 'react-native-slider'
 
-const SCREEN_HEIGHT = Dimensions.get('window').height
-const SCREEN_WIDTH = Dimensions.get('window').width
 
-const Logo = ({ scale }) => {
+const screen_height = Dimensions.get('window').height
+const screen_width = Dimensions.get('window').width
+const MusicPlayerScreen = () => {
+
+  const animatedValue = new Animated.ValueXY({ x: 0, y: screen_height - 80 })
+
+  const panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      animatedValue.extractOffset() //starts the panresponder from where we left, taking into account the offset
+    },
+    onPanResponderMove: (evt, gesture) => {
+      animatedValue.setValue({ x: 0, y: gesture.dy })
+    },
+    onPanResponderRelease: (evt, gesture) => {
+      if (gesture.dy < 0) {
+        Animated.spring(animatedValue.y, {
+          toValue: -screen_height + 100,
+          tension: 1
+        }).start()
+      }
+      if (gesture.dy > 0) {
+        Animated.spring(animatedValue.y, {
+          toValue: screen_height - 100,
+          tension: 1
+        }).start()
+      }
+    }
+  })
+  const animatedImageHeight = animatedValue.y.interpolate({
+    inputRange: [0, screen_height - 80],
+    outputRange: [150, 45],
+    extrapolate: 'clamp'
+  })
+  const animatedTextOpacity = animatedValue.y.interpolate({
+    inputRange: [0, screen_height - 500, screen_height - 80],
+    outputRange: [0, 0, 1],
+    extrapolate: 'clamp'
+  })
+  const animatedImageMargin = animatedValue.y.interpolate({
+    inputRange: [0, screen_height - 80],
+    outputRange: [screen_width / 2 - 70, 10],
+    extrapolate: 'clamp'
+  })
+  const animatedSliderHeight = animatedValue.y.interpolate({
+    inputRange: [0, screen_height - 80],
+    outputRange: [screen_height / 2, 80],
+    extrapolate: 'clamp'
+  })
+  const animatedSongDetailsOpacity = animatedValue.y.interpolate({
+    inputRange: [0, screen_height - 500, screen_height - 80],
+    outputRange: [1, 0, 0],
+    extrapolate: 'clamp'
+  })
+
+  const animatedHeight = {
+    transform: animatedValue.getTranslateTransform()
+  }
   return (
-    <Animated.View style={{ ...styles.logo, transform: [{ scale: scale }] }}>
-      <Text style={styles.logoText}>
-        Uber
-      </Text>
-    </Animated.View>
-  )
-}
-const Footer = () => {
-  return (
-    <View style={styles.footer}>
-
-    </View>
-  )
-}
-
-const App = () => {
-
-
-  const innerLoginY = interpolate(scaleAnimation, {  //interpolating the login view to move after the transition
-    inputRange: [0, 1],
-    outputRange: [150, 0],
-  })
-
-
-  //ALL GESTURE HANDLERS
-  const gestureState = useRef(new Animated.Value(State.UNDETERMINED))
-  const gestureHandler = onGestureEvent({ state: gestureState.current })
-
-  const backarrowstate = useRef(new Animated.Value(State.UNDETERMINED))
-  const backarrowhandler = onGestureEvent({ state: gestureState.current })
-
-  //ANIMATIONS
-  const isOpen = useRef(new Animated.Value(0))
-  const isOpenAnimation = withSpringTransition(isOpen.current, {
-    ...SpringUtils.makeDefaultConfig(),
-    overshootClamping: true,
-    damping: new Animated.Value(20)
-  })
-
-  const scale = useRef(new Animated.Value(0)); //define an animated value that maintains the state
-  const scaleAnimation = timing(scale.current, {
-    duration: 2000,
-    from: 0,
-    to: 1,
-    easing: Easing.linear    //creating the transition effect wrt the animated value
-  });
-
-  //useCode Hooks
-  //hook to implement this code immediately after screen renders
-
-
-  useCode(() => cond(eq(gestureState.current, State.END), [
-    cond(eq(isOpen.current, 0), set(isOpen.current, 1))
-  ]), [gestureState.current])
-  useCode(() => cond(eq(backarrowstate, State.END), [ //let the gesture run till the touch is released
-    set(gestureState.current, State.UNDETERMINED),  //disable the original touch gesture on the loginview, bring back to normal
-    set(isOpen.current, 0) //bring the open action sheet back down
-  ]), [backarrowstate.current])
-
-  useCode(() => cond(eq(scale.current, 0), set(scale.current, 1)), [])
-
-  //INTERPOLATIONS
-  const outerLoginY = interpolate(isOpenAnimation, {
-    inputRange: [0, 1],
-    outputRange: [SCREEN_HEIGHT - 150, 75]
-  })
-
-  const getmovingOpacity = interpolate(isOpenAnimation, {
-    inputRange: [0, 1],
-    outputRange: [1, 0]
-  })
-  const translateX = interpolate(isOpenAnimation, {
-    inputRange: [0, 1],
-    outputRange: [0, 25]
-  })
-  const translateY = interpolate(isOpenAnimation, {
-    inputRange: [0, 1],
-    outputRange: [0, -85]
-  })
-  const opacity = interpolate(isOpenAnimation, {
-    inputRange: [0, 0.5, 1],
-    outputRange: [0, 0, 1]
-  })
-
-
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Logo scale={scaleAnimation} />
+    <Animated.View style={styles.container}>
+      <View style={{ width: screen_width, height: 100, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ fontSize: 30, color: 'pink', letterSpacing: 1 }}>MUSIC PLAYER</Text>
       </View>
-      <TapGestureHandler {...backarrowhandler}>
-        <Animated.View style={{
-          opacity,
-          position: 'absolute',
-          top: 40,
-          left: 20
-        }}>
-          <Ionicons name="md-arrow-back" size={24} color="black" />
-        </Animated.View>
-      </TapGestureHandler>
       <Animated.View
-        style={{
-          backgroundColor: "white",
-          ...StyleSheet.absoluteFill,
-          transform: [{ translateY: outerLoginY }],
-        }}
-      >
-        <Animated.View
-          style={{
-            height: 150,
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "#2289d6",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            right: 0,
-          }}
-        >
+        {...panResponder.panHandlers}
+        style={[animatedHeight, {
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          zIndex: 10,
+          backgroundColor: 'white',
+          borderWidth: 1,
+          borderColor: 'lightgray',
+          height: screen_height,
+        }]}>
+        <Animated.View style={{ height: animatedSliderHeight, alignItems: 'center', flexDirection: 'row', width: '100%' }}>
+          <View style={{
+            flex: 4,
+            alignItems: 'center',
+            flexDirection: 'row',
+            height: 45,
+            width: 800
+          }}>
+            <Animated.View style={{
+              height: animatedImageHeight,
+              marginLeft: animatedImageMargin,
+              width: animatedImageHeight, borderRadius: 4,
+            }}>
+              <Image source={require('./assets/hotel.jpg')} style={{
+                flex: 1,
+                width: null,
+                height: null,
+                borderRadius: 2,
+                borderWidth: 1,
+                borderColor: 'black',
+              }} />
+            </Animated.View>
+            <Animated.Text style={{ opacity: animatedTextOpacity, fontSize: 17, letterSpacing: 0.8, marginLeft: 10 }}>Hotel California(Live)</Animated.Text>
+          </View>
+          <View style={{ width: 300, marginRight: 30, justifyContent: 'space-between', flexDirection: 'row', width: 40, height: 40, alignItems: 'center' }}>
+            <Animated.View style={{ marginRight: 5, opacity: animatedTextOpacity }}>
+              <TouchableOpacity>
+                <Ionicons name="ios-pause" size={40} color="black" />
+              </TouchableOpacity>
+            </Animated.View>
+            <Animated.View style={{ opacity: animatedTextOpacity }}>
+              <TouchableOpacity>
+                <AntDesign name="caretright" size={28} color="black" />
+              </TouchableOpacity>
+            </Animated.View>
+          </View>
         </Animated.View>
-        <Animated.View>
-          <Animated.View
-            style={{
-              height: 150,
-              backgroundColor: "white",
-              transform: [{ translateY: innerLoginY }],
-            }}
-          >
-            <Animated.View style={{
-              marginLeft: 25,
-              marginTop: 20,
-              alignItems: 'flex-start',
-              opacity: getmovingOpacity
-            }}>
-              <Text style={{
-                fontSize: 21,
-              }}>Get moving with Uber</Text>
-            </Animated.View>
-
-            <TapGestureHandler {...gestureHandler}>
-              <Animated.View>
-                <Animated.View style={{
-                  flexDirection: 'row',
-                  marginLeft: 25,
-                  marginTop: 15,
-                  alignItems: 'flex-start',
-                }} pointerEvents="none">
-                  <Animated.View style={{ justifyContent: 'center', alignItems: 'center', alignSelf: 'center' }}>
-                    <Text style={{ fontSize: 18 }}>+91-</Text>
-                  </Animated.View>
-                  <Animated.View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                    <TextInput keyboardType="number-pad"
-                      placeholder="Enter your mobile number"
-                      style={{
-                        marginLeft: 2,
-                        padding: 7,
-                        width: 300,
-                        fontSize: 17, borderWidth: 0.2, borderRadius: 5, borderColor: 'lightgray'
-                      }}
-                    />
-                  </Animated.View>
-                </Animated.View>
-              </Animated.View>
-            </TapGestureHandler>
-            <Animated.View style={{
-              opacity,
-              transform: [{
-                translateX,
-                translateY
-              }],
-              opacity,
-            }}>
-              <Text style={{ fontSize: 20, }}>Enter your mobile number</Text>
-            </Animated.View>
-          </Animated.View>
+        <Animated.View style={{ height: animatedSliderHeight, opacity: animatedSongDetailsOpacity }}>
+          <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
+            <Text style={{ fontSize: 22 }}>Hotel California (Live)</Text>
+            <Text style={{ fontSize: 18, color: 'pink' }}>Eagles - Hell Freezes Over</Text>
+          </View>
+          <View style={{ flex: 1, height: 40, alignItems: 'center', justifyContent: 'center' }}>
+            <Slider
+              style={{ width: 300, height: 40 }}
+              minimumValue={0}
+              maximumValue={1}
+              minimumTrackTintColor="#FFFFFF"
+              maximumTrackTintColor="#000000"
+            />
+          </View>
+          <View style={{
+            flex: 2, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'
+          }}>
+            <AntDesign name="stepbackward" size={36} color="black" />
+            <Ionicons name="ios-pause" size={64} color="black" />
+            <AntDesign name="stepforward" size={36} color="black" />
+          </View>
+          <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 15 }}>
+            <Ionicons name="ios-add" size={36} color="pink" style={{ fontWeight: "bold" }} />
+            <Ionicons name="ios-more" size={36} color="pink" style={{ fontWeight: "bold" }} />
+          </View>
         </Animated.View>
       </Animated.View>
-    </View>
+    </Animated.View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2289d6',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  logoText: {
-    fontSize: 30,
-    color: 'black'
-  },
-  logoContainer: {
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  logo: {
-    height: 100,
-    width: 100,
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white'
-  },
-  footer: {
-    width: SCREEN_WIDTH,
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    zIndex: 1
   }
 })
 
-export default App;
+export default MusicPlayerScreen;
